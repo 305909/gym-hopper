45c91e722f305e7ff1124a587904e68356479fc9
305909 <151930839+305909@users.noreply.github.com>
2024-06-27 13:11:41 +0200

    Initial commit

--- tunings/reinforce.py
+++ tunings/reinforce.py
@@ -1,9 +1,14 @@
 import os
+import sys
 import gym
 import argparse
 import warnings
 import itertools
 import numpy as np
+
+sys.path.append(
+	os.path.abspath(
+		os.path.join(os.path.dirname(__file__), '..')))
 
 from agents.reinforce import RF, RFPolicy
 from env.custom_hopper import *
