59796d545fbc186d29f3834895f614d6fea4e184
305909 <151930839+305909@users.noreply.github.com>
2024-06-27 15:28:17 +0200

    Initial commit

--- tunings/aac.py
+++ tunings/aac.py
@@ -10,7 +10,7 @@ sys.path.append(
 	os.path.abspath(
 		os.path.join(os.path.dirname(__file__), '..')))
 
-from agents.actor_critic import A2C, A2CPolicy
+from agents.aac import A2C, A2CPolicy
 from env.custom_hopper import *
 
 
