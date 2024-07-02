8e2a35ce54c1fede6bd93689f0a33373d549527a
305909 <151930839+305909@users.noreply.github.com>
2024-07-02 17:38:38 +0200

    Update aac.py

--- agents/aac.py
+++ agents/aac.py
@@ -84,11 +84,11 @@ class A2C:
     
     def __init__(self, policy, 
                  device: str = 'cpu',
-                 learning_rate: float = 1e-4,
+                 learning_rate: float = 1e-3,
                  max_grad_norm: float = 0.5,
                  entropy_coef: float = 0.0,
                  critic_coef: float = 0.5,
-                 batch_size: int = 16,
+                 batch_size: int = 32,
                  gamma: float = 0.99,
                  **kwargs):
                      
