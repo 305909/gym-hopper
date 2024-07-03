edab3a86c155bf4b0b0b6b310cd83bca0ac23b0f
305909 <151930839+305909@users.noreply.github.com>
2024-07-03 10:23:33 +0200

    Update rein.py

--- tunings/rein.py
+++ tunings/rein.py
@@ -102,14 +102,14 @@ def gridsearch(args, params, sessions = 5):
         res = np.mean(pool, axis = 0)
         cov = res.std() / res.mean()  # coefficient of variation
         score = res.mean() * (1 - cov)
-        print(f'\nScore: {score:.2f} | Avg. Reward: {res.mean():.2f} +/- {res.std():.2f} | Parameters: {kwargs}\n')
+        print(f'\nscore: {score:.2f} | reward: {res.mean():.2f} +/- {res.std():.2f} | parameters: {kwargs}\n')
         results.append([score, res.mean(), res.std(), kwargs])
 
     results.sort(key = lambda x: x[0], reverse = True)
-    print(f'\nGrid Search - Ranking Scores:')
+    print(f'\ngrid search - ranking scores:')
     print("---------------------------------------------")
     for rank, candidate in enumerate(results):
-        print(f'{rank + 1} | Score: {candidate[0]:.2f} | Avg. Reward: {candidate[1]:.2f} +/- {candidate[2]:.2f} | Parameters: {candidate[3]}')
+        print(f'{rank + 1} | score: {candidate[0]:.2f} | reward: {candidate[1]:.2f} +/- {candidate[2]:.2f} | parameters: {candidate[3]}')
         print("---------------------------------------------")
 
     return max(results, key = lambda x: x[0])
@@ -124,7 +124,7 @@ def main():
     
     prime = gridsearch(args, params)
     print("---------------------------------------------")
-    print(f'Maximum Score: {prime[0]:.2f} | Avg. Reward: {prime[1]:.2f} +/- {prime[2]:.2f} | Optimal Parameters: {prime[3]}')
+    print(f'maximum score: {prime[0]:.2f} | reward: {prime[1]:.2f} +/- {prime[2]:.2f} | optimal parameters: {prime[3]}')
 
 
 if __name__ == '__main__':
