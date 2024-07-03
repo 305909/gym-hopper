5fe6c067158ffb21b7821fa6e6ecf966065e2597
305909 <151930839+305909@users.noreply.github.com>
2024-07-03 10:33:51 +0200

    Update sac.py

--- tunings/sac.py
+++ tunings/sac.py
@@ -18,51 +18,27 @@ from env.custom_hopper import *
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train-env', 
-                        default = 'source', 
-			type = str,
-                        choices = ['source', 
-                                   'target',
-                                   'source-moderate-randomization', 
-                                   'target-moderate-randomization',
-                                   'source-wide-randomization', 
-                                   'target-wide-randomization'],
-                        help = 'Training environment')
-    parser.add_argument('--test-env', 
-                        default = 'target', 
-			type = str,
-                        choices = ['source', 
-                                   'target',
-                                   'source-moderate-randomization', 
-                                   'target-moderate-randomization',
-                                   'source-wide-randomization', 
-                                   'target-wide-randomization'],
-                        help = 'Testing environment')
-    parser.add_argument('--train-timesteps', 
-                        default = 100000, 
-			type = int, 
-                        help = 'Number of training timesteps')
-    parser.add_argument('--test-episodes', 
-                        default = 100, 
-			type = int, 
-                        help = 'Number of testing episodes')
-    parser.add_argument('--device', 
-                        default = 'cpu', 
-			type = str, 
-                        choices = ['cpu', 
-                                   'cuda'], 
-                        help = 'Network device [cpu, cuda]')
+    parser.add_argument('--train-env', default = 'source', type = str, 
+			help = 'Training environment')
+    parser.add_argument('--test-env', default = 'target', type = str, 
+			help = 'Testing environment')
+    parser.add_argument('--train-timesteps', default = 100000, type = int, 
+			help = 'Number of training timesteps')
+    parser.add_argument('--test-episodes', default = 100, type = int, 
+			help = 'Number of testing episodes')
+    parser.add_argument('--device', default = 'cpu', type = str, 
+			help = 'Network device [cpu, cuda]')
     return parser.parse_args()
 
 
 def train(device = 'cpu', 
           train_timesteps: int = 100000, 
           train_env: str = 'CustomHopper-source-v0', **kwargs) -> SAC:
-          
-    env = gym.make(train_env)
+    """ 
+        -> train the agent in the training environment
 
-    """ Training """
-              
+    """
+    env = gym.make(train_env)
     policy = 'MlpPolicy'
     agent = SAC(policy = policy, env = env, device = device, **kwargs)
     
@@ -74,11 +50,11 @@ def train(device = 'cpu',
 def test(agent: SAC, 
          test_episodes: int = 100, 
          test_env: str = 'CustomHopper-source-v0') -> float:
-             
+    """ 
+        -> test the agent in the testing environment
+        
+    """  
     env = gym.make(test_env)
-  
-    """ Evaluation """
-
     num_episodes = 0
     episode_rewards = []
     while num_episodes < test_episodes:
@@ -105,33 +81,38 @@ def pooling(kwargs: dict, device, train_timesteps, test_episodes, train_env, tes
     
     return test(agent, 
                 test_episodes = test_episodes, 
-                test_env = test_env)
+                test_env = test_env), kwargs
 
 
 
-def gridsearch(args, params, train_env, test_env):
+def gridsearch(args, params, train_env, test_env, sessions = 5):
     results = []
     keys = list(params.keys())
-    for param in itertools.product(*params.values()):
+    for param in list(itertools.product(*params.values())):
         kwargs = dict(zip(keys, param))
-        er = pooling(kwargs, 
-                     device = args.device,
-                     train_timesteps = args.train_timesteps,
-                     test_episodes = args.test_episodes, 
-                     train_env = train_env, 
-                     test_env = test_env)
-        cov = er.std() / er.mean()  # coefficient of variation
-        score = er.mean() * (1 - cov)
+        pool = list()
+        for iter in range(sessions):
+            er, _ = pooling(kwargs, 
+			    device = args.device,
+                            train_timesteps = args.train_timesteps,
+                            test_episodes = args.test_episodes, 
+			    train_env = trein_env, 
+			    test_env = test_env)
+            pool.append(er)
+        pool = np.array(pool)
+        res = np.mean(pool, axis = 0)
+        cov = res.std() / res.mean()  # coefficient of variation
+        score = res.mean() * (1 - cov)
         print("---------------------------------------------")
-        print(f'Score: {score:.2f} | Avg. Reward: {er.mean():.2f} +/- {er.std():.2f} | Parameters: {kwargs}')
-        results.append([score, er.mean(), er.std(), kwargs])
+        print(f'score: {score:.2f} | reward: {res.mean():.2f} +/- {res.std():.2f} | parameters: {kwargs}')
+        results.append([score, res.mean(), res.std(), kwargs])
 
     results.sort(key = lambda x: x[0], reverse = True)
     print("---------------------------------------------")
-    print(f'Grid Search - Ranking Scores:')
+    print(f'grid search - ranking scores:')
     print("---------------------------------------------")
     for rank, candidate in enumerate(results):
-        print(f'{rank + 1} | Score: {candidate[0]:.2f} | Avg. Reward: {candidate[1]:.2f} +/- {candidate[2]:.2f} | Parameters: {candidate[3]}')
+        print(f'{rank + 1} | score: {candidate[0]:.2f} | reward: {candidate[1]:.2f} +/- {candidate[2]:.2f} | parameters: {candidate[3]}')
         print("---------------------------------------------")
 
     return max(results, key = lambda x: x[0])
@@ -148,7 +129,7 @@ def main():
     
     prime = gridsearch(args, params, train_env, test_env)
     print("---------------------------------------------")
-    print(f'Maximum Score: {prime[0]:.2f} | Avg. Reward: {prime[1]:.2f} +/- {prime[2]:.2f} | Optimal Parameters: {prime[3]}')
+    print(f'maximum score: {prime[0]:.2f} | reward: {prime[1]:.2f} +/- {prime[2]:.2f} | optimal parameters: {prime[3]}')
     print("---------------------------------------------")
 
 
