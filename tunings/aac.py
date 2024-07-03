2fc653fff07a8a985f8d5f593d0944b2bc80975b
305909 <151930839+305909@users.noreply.github.com>
2024-07-03 17:06:23 +0200

    Update aac.py

--- tunings/aac.py
+++ tunings/aac.py
@@ -105,17 +105,14 @@ def gridsearch(args, params, sessions = 5):
         res = np.mean(pool, axis = 0)
         cov = res.std() / res.mean()  # coefficient of variation
         score = res.mean() * (1 - cov)
-        print("---------------------------------------------")
         print(f'score: {score:.2f} | reward: {res.mean():.2f} +/- {res.std():.2f} | parameters: {kwargs}')
         results.append([score, res.mean(), res.std(), kwargs])
 
     results.sort(key = lambda x: x[0], reverse = True)
-    print("---------------------------------------------")
-    print(f'grid search - ranking scores:')
-    print("---------------------------------------------")
+    print(f'\ngrid search - ranking scores:')
+    print("-----------------------------")
     for rank, candidate in enumerate(results):
         print(f'{rank + 1} | score: {candidate[0]:.2f} | reward: {candidate[1]:.2f} +/- {candidate[2]:.2f} | parameters: {candidate[3]}')
-        print("---------------------------------------------")
 
     return max(results, key = lambda x: x[0])
 
@@ -133,9 +130,8 @@ def main():
         args.device = 'cpu'
 	    
     prime = gridsearch(args, params)
-    print("---------------------------------------------")
-    print(f'maximum score: {prime[0]:.2f} | reward: {prime[1]:.2f} +/- {prime[2]:.2f} | optimal parameters: {prime[3]}')
-    print("---------------------------------------------")
+    print(f'\nmaximum score: {prime[0]:.2f} | reward: {prime[1]:.2f} +/- {prime[2]:.2f} | optimal parameters: {prime[3]}')
+    print("--------------")
 
 
 if __name__ == '__main__':
