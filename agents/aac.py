6310d55154b469047d9378c9881c620443a146ea
305909 <151930839+305909@users.noreply.github.com>
2024-07-06 11:37:04 +0200

    Update aac.py

--- agents/aac.py
+++ agents/aac.py
@@ -7,22 +7,32 @@ from torch.distributions import Normal
 
 class Policy(torch.nn.Module):
     
-    def __init__(self, state_space, action_space, actor = True, **kwargs):
+    def __init__(self, state_space: int, action_space: int, actor = True, **kwargs):
         super().__init__()
-        
+        """ initializes a neural network that estimates 
+        the mean and standard deviation of a normal distribution 
+        from which to sample an action (actor), or 
+        the mean of a normal distribution (critic)
+               
+        args:
+            state_spaces: dimension of the observation space
+            action_space: dimension of the action space
+            actor: boolean parameter to set the actor and critic
+        """
         self.action_space = action_space
         self.state_space = state_space
         self.tanh = torch.nn.Tanh()
         self.actor = actor
         self.hidden = 64
+        self.eps = 1e-6
 
-        """ Actor/Critic network """
+        """ policy mean specific layers """
         self.fc1 = torch.nn.Linear(state_space, self.hidden)
         self.fc2 = torch.nn.Linear(self.hidden, self.hidden)
         self.fc3 = torch.nn.Linear(self.hidden, action_space)
 
         if actor:
-            # standard deviation for exploration at training time
+            """ policy standard deviation specific layer """
             init_sigma = 0.5
             self.sigma = torch.nn.Parameter(torch.zeros(self.action_space) + init_sigma)
             self.sigma_activation = F.softplus
@@ -36,7 +46,20 @@ class Policy(torch.nn.Module):
                 torch.nn.init.zeros_(m.bias)
 
     def forward(self, x):
-        """ Actor/Critic """
+        """ from the observation x, 
+        returns the normal distribution 
+        from which to sample an action (actor), or 
+        the mean of a normal distribution (critic)
+
+        args:
+            x: observation from the environment
+
+        returns:
+            (actor) normal_dist: normal distribution N(μ,σ)
+                                 - μ: action mean
+                                 - σ: action standard deviation
+            (critic) action_mean: action mean μ
+        """
         x = self.tanh(self.fc1(x))
         x = self.tanh(self.fc2(x))
         action_mean = self.fc3(x)
@@ -49,7 +72,7 @@ class Policy(torch.nn.Module):
             return action_mean
 
     def to(self, device):
-        # move parameters to device
+        """ move parameters to device """
         for param in self.parameters():
             param.data = param.data.to(device)
         return self
@@ -58,13 +81,19 @@ class Policy(torch.nn.Module):
 class A2CPolicy:
     
     def __init__(self, state_space, action_space, **kwargs):
-
+        """ initialize a neural network for the actor 
+        and a neural network for the critic
+        
+        args:
+            state_spaces: dimension of the observation space
+            action_space: dimension of the action space
+        """
         self.policies = OrderedDict()
         self.policies['actor'] = Policy(state_space, action_space)
         self.policies['critic'] = Policy(state_space, 1, actor = False)
 
     def to(self, device):
-        # move parameters to device
+        """ move parameters to device """
         for k, v in self.policies.items():
             self.policies[k] = v.to(device)
         return self
@@ -91,14 +120,24 @@ class A2C:
                  batch_size: int = 32,
                  gamma: float = 0.99,
                  **kwargs):
-                     
+        """ initializes an agent that learns a policy via Advantage Actor-Critic algorithm 
+        to solve the task at hand (Gym Hopper)
+
+        args:
+            policy: A2CPolicy
+            device: processing device (e.g. CPU or GPU)
+            learning_rate: learning rate for policy optimization
+            max_grad_norm: threshold value for the gradient norm
+            entropy_coef: entropy coefficient to balance exploration/exploitation
+            critic_coef: critic coefficient to weight the value function loss
+            batch_size: number of time steps to collect before updating the policy
+            gamma: discount factor
+        """      
         self.device = device
         self.policy = policy.to(self.device)
 
         self.max_grad_norm = max_grad_norm
-        # entropy coefficient to balance exploration/exploitation
         self.entropy_coef = entropy_coef
-        # critic coefficient to weight the value function loss
         self.critic_coef = critic_coef
 
         self.learning_rate = learning_rate
@@ -111,15 +150,37 @@ class A2C:
         self.reset()
 
     def predict(self, obs, state = None, episode_start = None, deterministic = False):
+        """ predicts an action, according to the policy and the observation
+
+        args:
+            obs: observation from the environment
+
+        returns:
+            action: action to perform
+            action_log_prob: logarithmic probability value of the action
+            state_value: critic's estimation of the value of the current state
+        """
         x = torch.from_numpy(obs).float().to(self.device)
         normal_dist = self.policy.policies['actor'](x)
 
-        if deterministic:  # return mean
+        if deterministic:
+            """ return the mean 
+            of the normal distribution N(μ,σ)
+            
+            returns:
+                a = μ
+            """
             action = normal_dist.mean
             action = action.detach().cpu().numpy()
             return action, None
             
-        else:  # sample from the distribution
+        else:
+            """ sample an action 
+            from the normal distribution N(μ,σ)
+
+            returns:
+                a ~ N(μ,σ)
+            """
             action = normal_dist.sample()
             action_log_prob = normal_dist.log_prob(action).sum()
             state_value = self.policy.policies['critic'](x)
@@ -134,35 +195,37 @@ class A2C:
         self.state_values.append(state_value)
 
     def update_policy(self):
-        # stack and move data to device
+        """ stack and move data to device """
         states = torch.stack(self.states, dim = 0).to(self.device).squeeze(-1)
         action_log_probs = torch.stack(self.action_log_probs, dim = 0).to(self.device).squeeze(-1)
         rewards = torch.tensor(self.rewards, dtype = torch.float32).to(self.device)
         dones = torch.tensor(self.dones, dtype = torch.float32).to(self.device)
         state_values = torch.stack(self.state_values, dim = 0).to(self.device).squeeze(-1)
 
-        # compute bootstrapped returns
+        """ compute bootstrapped returns """
         bootstrapped_returns = torch.zeros_like(rewards)
         cumulative = 0
         for t in reversed(range(len(rewards))):
             cumulative = rewards[t] + self.gamma * cumulative * (1 - dones[t])
             bootstrapped_returns[t] = cumulative
 
-        # compute advantages
+        """ compute advantages via GAE """
         advantages = bootstrapped_returns - state_values
         advantages = (advantages - advantages.mean()) / (advantages.std(unbiased = False) + 1e-5)
 
-        # compute actor loss - with entropy regularization to encourage exploration
+        """ compute actor loss 
+        with entropy regularization to encourage exploration """
         entropy = self.policy.policies['actor'](states).entropy().mean()
         actor_loss = - (action_log_probs * advantages.detach()).mean() - self.entropy_coef * entropy
 
-        # compute critic loss
+        """ compute critic loss """
         critic_loss = F.mse_loss(state_values, bootstrapped_returns.detach())
 
-        # compute total loss with value function loss weight
+        """ compute total loss 
+        with value function loss weight """
         loss = actor_loss + self.critic_coef * critic_loss
         
-        # optimize actor/critic
+        """ updates the policy network's weights """
         self.optimizer.zero_grad()
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.policy.policies['actor'].parameters(), self.max_grad_norm)
