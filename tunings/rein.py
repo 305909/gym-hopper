30f0766d2d4b318c443fe769d3189c1255b91bcc
305909 <151930839+305909@users.noreply.github.com>
2024-07-07 11:34:20 +0200

    Update rein.py

--- tunings/rein.py
+++ tunings/rein.py
@@ -131,7 +131,7 @@ def main():
     args = parse_args()
     warnings.filterwarnings("ignore")
     params = {
-        'learning_rate': [1e-3, 1e-4, 1e-5]
+        'learning_rate': [7e-4, 3e-4]
         }
 	
     if args.device == 'cuda' and not torch.cuda.is_available():
