de9c6f62672c53a2f52fda342f55ebf8d0309f6c
305909 <151930839+305909@users.noreply.github.com>
2024-07-07 11:34:15 +0200

    Update sac.py

--- tunings/sac.py
+++ tunings/sac.py
@@ -133,7 +133,7 @@ def main():
     args = parse_args()
     warnings.filterwarnings("ignore")
     params = {
-        'learning_rate': [1e-3, 7e-4, 5e-4, 3e-4, 1e-4]
+        'learning_rate': [7e-4, 3e-4]
         }
     
     train_env, test_env = tuple(f'CustomHopper-{x}-v0' 
