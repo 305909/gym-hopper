c9c1b91b9ee44b2ab2cc5287d34a17d0d2536bb1
305909 <151930839+305909@users.noreply.github.com>
2024-07-13 18:57:15 +0200

    Update PPO.py

--- algorithms/PPO.py
+++ algorithms/PPO.py
@@ -252,7 +252,7 @@ def main():
         policy = 'MlpPolicy'
         agent = PPO(policy, env = env, device = args.device)
         
-        agent.policy.load_state_dict(weights)
+        agent.policy.load_state_dict(pool['weights'][0])
         agent.save(f'{args.directory}/PPO-({args.train_env} to {args.test_env}).mdl')
         print(f'\nmodel checkpoint storage: {args.directory}/PPO-({args.train_env} to {args.test_env}).mdl\n')
         
