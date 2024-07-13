5c5162d38cc0a96a623d293720efbdc0cf03ddfb
305909 <151930839+305909@users.noreply.github.com>
2024-07-13 11:38:43 +0200

    Update custom_hopper.py

--- env/custom_hopper.py
+++ env/custom_hopper.py
@@ -62,7 +62,7 @@ class CustomHopper(MujocoEnv, utils.EzPickle):
         return masses
 
     def set_parameters(self, *task):
-        self.sim.model.body_mass[2:] = task
+        self.sim.model.body_mass[2:] = np.array(task)
 
     def step(self, a):
         """ step the simulation to the next timestep
