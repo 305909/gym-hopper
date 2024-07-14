a51f39072fb7e3596c26152a471f7d2186f5b67e
305909 <151930839+305909@users.noreply.github.com>
2024-07-14 15:21:54 +0200

    Update and rename REINFORCE.py to MCPG.py

--- algorithms/MCPG.py
+++ algorithms/MCPG.py
@@ -26,7 +26,7 @@ from env.custom_hopper import *
 
 from utils import Callback
 from collections import OrderedDict
-from agents.reinforce import RF, RFPolicy
+from agents.mc import RF, RFPolicy
 from utils import display, multiprocess, stack, track
 from stable_baselines3.common.evaluation import evaluate_policy
 
