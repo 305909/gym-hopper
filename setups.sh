105f4f3e798b90bb4737d68309a30e257422f4ef
305909 <151930839+305909@users.noreply.github.com>
2024-07-14 15:54:25 +0200

    Update setups.sh

--- setups.sh
+++ setups.sh
@@ -4,12 +4,15 @@
 apt-get update && apt-get install -y \
     libgl1-mesa-dev \
     libgl1-mesa-glx \
-    libglew-dev \
-    libosmesa6-dev \
+    libglew-dev
+    
+apt-get update && apt-get install -y \
     software-properties-common \
+    libosmesa6-dev \
     patchelf
 
 # install python packages
 pip install gym
 pip install free-mujoco-py
+pip install sb3-contrib
 pip install "stable-baselines3[extra]>=2.0.0a4"
