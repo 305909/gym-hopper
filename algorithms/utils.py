d039fafff7e94d832fa9ee8678c4baea8f6ca618
305909 <151930839+305909@users.noreply.github.com>
2024-07-14 18:52:10 +0200

    Update utils.py

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -152,7 +152,7 @@ def collect(env, seed, maxit = 10):
 
 def optimize_params(real_data, sim_data, seed, maxit: int = 100, learning_rate: int = 0.001):
     parts = ['torso', 'thigh', 'leg', 'foot']
-    masses = np.array([2.53429174 3.92699082 2.71433605 5.0893801])  # initial guess for link masses
+    masses = np.array([2.53429174, 3.92699082, 2.71433605, 5.0893801])  # initial guess for link masses
     print(f'initial guess for physical parameters:')
     print(f'-------------------------------------')
     for part, mass in zip(parts, masses):
