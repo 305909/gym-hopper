d84f1ff2eb5ebd253834202cdc0cbc622a2088c4
305909 <151930839+305909@users.noreply.github.com>
2024-07-14 21:20:31 +0200

    Update DROID.py

--- algorithms/DROID.py
+++ algorithms/DROID.py
@@ -118,7 +118,7 @@ def train(args, seed, train_env, test_env, model):
     sim_data = collect(gym.make('CustomHopper-source-UDR-v0'), seed)
     
     # optimize the masses
-    masses = optimize_params(real_data, sim_data, seed, maxit = 100, learning_rate = 1e-3)
+    masses = optimize_params(real_data, sim_data, seed, maxit = 100, learning_rate = 1e-2)
     
     env = gym.make(train_env, params = masses)
     env.set_randomness(args.dist)
