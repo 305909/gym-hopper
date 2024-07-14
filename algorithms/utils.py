b73768b81619aa026a2f0bf73291313c6db1e1d2
305909 <151930839+305909@users.noreply.github.com>
2024-07-14 19:09:01 +0200

    Update utils.py

--- algorithms/utils.py
+++ algorithms/utils.py
@@ -150,7 +150,7 @@ def collect(env, seed, maxit = 10):
     return data
 
 
-def optimize_params(real_data, sim_data, seed, maxit: int = 100, learning_rate: int = 0.001):
+def optimize_params(real_data, sim_data, seed, maxit: int = 100, learning_rate: float = 1e-3):
     parts = ['torso', 'thigh', 'leg', 'foot']
     masses = np.array([2.53429174, 3.92699082, 2.71433605, 5.0893801])  # initial guess for link masses
     print(f'initial guess for physical parameters:')
@@ -159,9 +159,9 @@ def optimize_params(real_data, sim_data, seed, maxit: int = 100, learning_rate: 
         print(f'{part}: {mass}')
 
     def compute_loss(real_data, sim_data, masses):
-        real_obs = np.array([np.sum([step[0] for step in episode]) for episode in real_data])
-        sim_obs = np.array([np.sum([step[0] for step in episode]) for episode in sim_data])
-        return np.mean((real_obs - sim_obs) ** 2)
+        real_rewards = np.array([np.sum([step[2] for step in episode]) for episode in real_data])
+        sim_rewards = np.array([np.sum([step[2] for step in episode]) for episode in sim_data])
+        return np.mean((real_rewards - sim_rewards) ** 2)
 
     for iter in range(maxit):
         losses = list()
