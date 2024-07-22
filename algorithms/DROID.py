6d9d415840f92dba3605eddf362d5d75c2f8ff1d
305909 <151930839+305909@users.noreply.github.com>
2024-07-22 10:58:21 +0200

    Update DROID.py

--- algorithms/DROID.py
+++ algorithms/DROID.py
@@ -261,7 +261,7 @@ def main():
         policy = 'MlpPolicy'
         agent = PPO(policy, env = env, device = args.device)
         
-        agent.policy.load_state_dict(pool['weights'][np.argmax(pool['rewards'])])
+        agent.policy.load_state_dict(pool['weights'][np.argmax([rewards[-1] for rewards in pool['rewards']])])
         agent.save(f'{args.directory}/DROID.mdl')
         print(f'\nmodel checkpoint storage: {args.directory}/DROID.mdl\n')
         
